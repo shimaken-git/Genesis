632fc808592801b6644fc8d33a9d385ca03fdb56
shimaken-git <shimaken@ba.mbn.or.jp>
2025-03-19 00:59:16 +0900

    update

--- examples/locomotion/go2_eval.py
+++ examples/locomotion/go2_eval.py
@@ -40,6 +40,7 @@ def main():
     rospy.init_node('genesis_eval_node')
     rospy.Subscriber("/realRobot/imu", Imu, imuCb)
     rospy.Subscriber("/cmd_vel", Twist, twistCb)
+    rospy.Subscriber("/realRobot/joint_states", JointState, jointCb)
     pub = rospy.Publisher("/genesis_angles", Float32MultiArray, queue_size=1)
 
     gs.init(backend=gs.cpu)
@@ -64,9 +65,9 @@ def main():
     policy = runner.get_inference_policy(device="cpu")
 
     obs, _ = env.reset()
-    for i in range(12):
-        joint.position.append(0)
-        joint.velocity.append(0)
+    # for i in range(12):
+    #     joint.position.append(0)
+    #     joint.velocity.append(0)
     angle_msg = Float32MultiArray()
     projected_gravity = torch.zeros((1, 3), device="cpu", dtype=gs.tc_float)
     with torch.no_grad():
@@ -76,20 +77,20 @@ def main():
             inv_base_quat = inv_quat(torch.tensor([[1.0, imu.orientation.x, imu.orientation.y, imu.orientation.z]]))
             projected_gravity = transform_by_quat(env.global_gravity, inv_base_quat)
             # obs[0, 0:3] = torch.tensor([imu.angular_velocity.x, imu.angular_velocity.y, imu.angular_velocity.z]) * env.obs_scales["ang_vel"]
-            obs[0, 3:6] = projected_gravity
-            obs[0, 6:9] = torch.tensor([cmd_vel.linear.x * 2.0, 0.0, cmd_vel.angular.z]) * env.commands_scale
-            if len(joint.position) == 12:
-                obs[0, 9:21] = (torch.tensor(joint.position) - env.default_dof_pos) * env.obs_scales["dof_pos"]
-                obs[0, 21:33] = torch.tensor(joint.velocity) * env.obs_scales["dof_vel"]
+            # obs[0, 3:6] = projected_gravity
+            obs[0, 6:9] = torch.tensor([cmd_vel.linear.x * 2.0, 0.0, cmd_vel.angular.z * 1.5]) * env.commands_scale
+            # if len(joint.position) == 12:
+            #     obs[0, 9:21] = (torch.tensor(joint.position) - env.default_dof_pos) * env.obs_scales["dof_pos"]
+            #     obs[0, 21:33] = torch.tensor(joint.velocity) * env.obs_scales["dof_vel"]
             # obs[0, 33:45] = actions
-            print(obs)
+            # print(obs)
             # commands
-            print(obs[0, 6:9])  #commands
+            # print(obs[0, 6:9])  #commands
             ## make actions
             actions = policy(obs)
             obs, _, rews, dones, infos = env.step(actions)
-            print("base_quat ", env.base_quat)
             ## make publish data
+            angle_msg.data.clear()
             for i in range(12):
                 angle_msg.data.append(env.target_dof_pos[0, i].item())
             pub.publish(angle_msg)
